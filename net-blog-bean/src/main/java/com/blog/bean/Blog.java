6920dbd9b7523f1129288a46f9c15a3c0e4aa859
meteorst <1214045596@qq.com>
2019-09-23 15:15:56 +0800

    âadd mybatis generator

---
+++ net-blog-bean/src/main/java/com/blog/bean/Blog.java
@@ -0,0 +1,115 @@
+package com.blog.bean;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Table(name = "blog")
+public class Blog implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    /**
+     * 标题
+     */
+    @Column(name = "title")
+    private String title;
+
+    /**
+     * 摘要
+     */
+    @Column(name = "summary")
+    private String summary;
+
+    /**
+     * 内容
+     */
+    @Column(name = "content")
+    private String content;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取标题
+     *
+     * @return title - 标题
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * 设置标题
+     *
+     * @param title 标题
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * 获取摘要
+     *
+     * @return summary - 摘要
+     */
+    public String getSummary() {
+        return summary;
+    }
+
+    /**
+     * 设置摘要
+     *
+     * @param summary 摘要
+     */
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    /**
+     * 获取内容
+     *
+     * @return content - 内容
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * 设置内容
+     *
+     * @param content 内容
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", title=").append(title);
+        sb.append(", summary=").append(summary);
+        sb.append(", content=").append(content);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ net-blog-dao/src/main/java/com/blog/dao/BlogDao.java
@@ -0,0 +1,16 @@
+package com.blog.dao;
+
+import com.blog.bean.Blog;
+
+/**
+* 通用 Mapper 代码生成器
+*
+* @author mapper-generator
+*/
+public interface BlogDao extends tk.mybatis.mapper.common.Mapper<Blog> {
+
+}
+
+
+
+
